9af8e8f24ab05b8fb69f628359cda84b9b6eb273
Jordan Foster <jordan.foster11@gmail.com>
2017-12-21 18:07:51 -0500

    Added mobile nav functionality

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
+
+const mobileNavCollapseClasses = {
+    menuHidden: 'mobile-nav-collapse hidden',
+    menuExpanded: 'mobile-nav-collapse expanded'
+}
+const mobileMaxWidth = 992;
+const mobileNavItemTarget = 'mobile-nav-item';
+const mobileNavButtonTarget = 'nav-dropdown-button icon-bar';
 
 class Header extends Component {
-
     constructor() {
         super();
 
@@ -11,34 +19,48 @@ class Header extends Component {
                 "ABOUT",
                 "CONTACT"
             ],
-            wrapperRef: {},
-            showMobileNavMenu: false
+            mobileNavMenuClass: mobileNavCollapseClasses.menuHidden
         }
     }
 
-    buildNavLinks = (isMobile) => {
-        let listClass = isMobile ? 'mobile-nav-menu' : 'nav-menu';
-        let navItemClass = isMobile ? 'mobile-nav-item mobile-nav-item-' : 'nav-item nav-item-';
-
-        const navItems = this.state.navLinks.map((link, index) => {
-            navItemClass += link.toLowerCase();
-            return <li className={navItemClass} key={index}>{link}</li>
-        });
-        return <ul className={listClass}>{navItems}</ul>
+    componentDidMount() {
+        window.addEventListener('resize', this.handleResize, false);
     }
 
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleResize, false);
+    }
+
+    /*
+    * Used to hide open mobile nav menu when window width is greater than 992px
+    */
+    handleResize = () => {
+        if ($(window).width() > mobileMaxWidth && this.state.mobileNavMenuClass.indexOf(mobileNavCollapseClasses.menuExpanded) > -1) {
+            this.setState({mobileNavMenuClass: mobileNavCollapseClasses.menuHidden});
+        }
+    }
+
+    /*
+    * Toggles the mobile nav menu on click of the mobile nav menu button
+    */
     toggleMobileNavMenu = () => {
-        document.addEventListener('mousedown', (e) => this.handleClickOutside(e));
-        this.setState({showMobileNavMenu: !this.state.showMobileNavMenu});
+        if (this.state.mobileNavMenuClass.indexOf(mobileNavCollapseClasses.menuHidden) > -1) {
+            this.setState({mobileNavMenuClass: mobileNavCollapseClasses.menuExpanded});
+            document.addEventListener('mousedown', this.handleClickOutside, false);
+        } else {
+            this.setState({mobileNavMenuClass: mobileNavCollapseClasses.menuHidden});
+        }
     }
 
+    /*
+    * Toggles the mobile nav menu when clicking outside of the mobile nav menu button or mobile nav menu
+    */
     handleClickOutside = (e) => {
-         if (e.target.className.indexOf('mobile-nav-item') > -1 && this.state.showMobileNavMenu) {
-            console.log('clicked');
-            document.removeEventListener('mousedown', (e) => this.handleClickOutside(e));
-            this.setState({showMobileNavMenu: false});
-        } else {
-            console.log('didnt click');
+        if (mobileNavItemTarget.indexOf(e.target.className) < 0) {
+            document.removeEventListener('mousedown', this.handleClickOutside, false);
+            if (mobileNavButtonTarget.indexOf(e.target.className) < 0) {
+                this.setState({mobileNavMenuClass: mobileNavCollapseClasses.menuHidden});
+            }
         }
     }
 
@@ -48,7 +70,13 @@ class Header extends Component {
                 <div className="nav-header">
                         <div className="header-title">JORDAN FOSTER</div>
                         <div className="navigation">
-                            {this.buildNavLinks(false)}
+                            <ul className="nav-menu">
+                                {
+                                    this.state.navLinks.map((link, index) => {
+                                        return <li className="nav-item" key={index}>{link}</li>
+                                    })
+                                }
+                            </ul>
                         </div>
                         <div className="navigation-dropdown">
                             <button className="nav-dropdown-button" onClick={this.toggleMobileNavMenu}>
@@ -58,10 +86,15 @@ class Header extends Component {
                             </button>
                         </div>
                 </div>
-                {
-                    this.state.showMobileNavMenu ? <div className="mobile-nav-collapse">{this.buildNavLinks(true)}</div> : null
-                }
-                {/* <div ref={this.setWrapperRef} className="mobile-nav-collapse">{this.buildNavLinks(true)}</div> */}
+                <div ref="mobile-nav-collapse" className={this.state.mobileNavMenuClass}>
+                    <ul className="mobile-nav-list">
+                        {
+                            this.state.navLinks.map((link, index) => {
+                                return <li className="mobile-nav-item" key={index}>{link}</li>
+                            })
+                        }
+                    </ul>
+                </div>
             </header>
         );
     }
