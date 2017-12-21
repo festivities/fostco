efdc2f6f03d7a04314ba8d177dc60412257d3064
Jordan <jordan.foster11@gmail.com>
2017-12-20 19:25:11 -0800

    bootstrap changes

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -24,13 +24,13 @@ class Header extends Component {
 
     render() {
         return (
-            <header className="Header container">
-                <div className="row">
-                    <div className="header-title col-md-2">JORDAN FOSTER</div>
-                    <div className="navigation col-5 col-lg-5">
+            <header className="Header container-fluid">
+                <div className="row header-row">
+                    <div className="header-title col-xs-10 col-sm-10 col-md-7 col-lg-7">JORDAN FOSTER</div>
+                    <div className="navigation col-md-5 col-lg-5">
                         {this.buildNavLinks()}
                     </div>
-                    <div className="navigation-dropdown col-xs-3 col-sm-3 col-md-10">
+                    <div className="navigation-dropdown col-xs-2 col-sm-2 col-md-3">
                         <button className="nav-dropdown-button">
                             <span className="icon-bar"></span>
                             <span className="icon-bar"></span>
