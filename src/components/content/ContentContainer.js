f85bc96425ad93f8fba895603741b47d2cfc7799
Jordan Foster <jordan.foster11@gmail.com>
2017-12-22 18:10:18 -0500

    added views and reworked footer

--- src/components/content/ContentContainer.js
+++ src/components/content/ContentContainer.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Home from './views/home/Home';
 
 class ContentContainer extends Component {
 
     render() {
         return (
             <div className="Content-container">
+                <div className="content">
+                    <Home />
+                </div>
             </div>
         );
     }
---
+++ src/components/content/views/home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+
+    render() {
+        return (
+            <div className="HomePage">
+                <div className="page-title">Home</div>
+            </div>
+        )
+    }
+}
+
+export default Home;
