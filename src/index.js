9bba2b1f0e506cd1efee6396237339c5e7347bd7
Jordan Foster <jordan.foster11@gmail.com>
2018-01-08 17:52:58 -0500

    prod build #1

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import './sass/Index.scss';
-// import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
     <BrowserRouter>
-        {/* <App /> */}
         <Route path="/" component={App}/>
     </BrowserRouter>, 
     document.getElementById('root')
