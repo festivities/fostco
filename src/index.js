f637e4c5794ffa1c51afd2a6fcc99a37f0223d89
Jordan <jordan.foster11@gmail.com>
2018-01-24 17:06:46 -0800

    Set history to be conditional depending on how app opens

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
 
 import './styles/sass/index.scss';
 
 import App from './components/App';
+import configureHistory from './configureHistory';
 import registerServiceWorker from './registerServiceWorker';
 
+const history = configureHistory();
+
 ReactDOM.render(
-    <BrowserRouter>
+    <Router history={history}>
         <Route path={process.env.PUBLIC_URL + '/'} component={App}/>
-    </BrowserRouter>,
+    </Router>,
     document.getElementById('root')
 );
 registerServiceWorker();
